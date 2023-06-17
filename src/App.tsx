0b2c106763b1c8543762a05bd4537789afee51a5
lilman109 <akira.yoshidasjsu@gmail.com>
2023-06-17 21:54:35 +0900

    register form contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
-import YouTubeForm from './components/YouTubeForm';
+import React from "react";
+import "./App.css";
+import YouTubeForm from "./components/YouTubeForm";
 
 function App() {
+
   return (
-  <div>
-      <YouTubeForm/>
+    <div>
+      <YouTubeForm />
     </div>
   );
 }
