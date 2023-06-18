5a5c54d0e06b8dd2c9e6f53231ff1975af2ab3b2
lilman109 <akira.yoshidasjsu@gmail.com>
2023-06-18 14:14:23 +0900

    added at least one phnumber is required and error messages for social

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 type FormValues = {
@@ -11,6 +11,9 @@ type FormValues = {
     facebook: string;
   };
   phoneNumbers: string[];
+  phnNumbers: {
+    number: string;
+  }[];
 };
 
 const YouTubeForm = () => {
@@ -24,10 +27,16 @@ const YouTubeForm = () => {
         facebook: "",
       },
       phoneNumbers: ["", ""],
+      phnNumbers: [{ number: "" }],
     },
   });
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
+
+  const { fields, append, remove } = useFieldArray({
+    name: "phnNumbers",
+    control,
+  });
 
   const onSubmit = (data: FormValues) => {
     console.log("form submitted", data);
@@ -100,22 +109,32 @@ const YouTubeForm = () => {
 
         <div className="font-control">
           <label htmlFor="twitter">Twitter</label>
-          <input type="text" id="twitter" {...register("social.twitter", {
-            required: {
-              value: true,
-              message: "Twitter is required"
-            }
-          })} />
+          <input
+            type="text"
+            id="twitter"
+            {...register("social.twitter", {
+              required: {
+                value: true,
+                message: "Twitter is required",
+              },
+            })}
+          />
+          <p className="error">{errors.social?.twitter?.message}</p>
         </div>
 
         <div className="font-control">
           <label htmlFor="facebook">Facebook</label>
-          <input type="text" id="facebook" {...register("social.facebook", {
-            required: {
-              value: true,
-              message: ""
-            }
-          })} />
+          <input
+            type="text"
+            id="facebook"
+            {...register("social.facebook", {
+              required: {
+                value: true,
+                message: "Facebook is required",
+              },
+            })}
+          />
+          <p className="error">{errors.social?.facebook?.message}</p>
         </div>
 
         <div className="font-control">
@@ -126,10 +145,13 @@ const YouTubeForm = () => {
             {...register("phoneNumbers.0", {
               required: {
                 value: true,
-                message: "Primary phone number is required"
-              }
+                message: "Primary phone number is required",
+              },
             })}
           />
+          {errors.phoneNumbers?.[0]?.message && (
+            <p className="error">{errors.phoneNumbers?.[0]?.message}</p>
+          )}
         </div>
 
         <div className="font-control">
@@ -140,10 +162,48 @@ const YouTubeForm = () => {
             {...register("phoneNumbers.1", {
               required: {
                 value: true,
-                message: "Secondary phone number is required"
-              }
+                message: "Secondary phone number is required",
+              },
             })}
           />
+          {errors.phoneNumbers?.[1]?.message && (
+            <p className="error">{errors.phoneNumbers?.[1]?.message}</p>
+          )}
+        </div>
+
+        <div>
+          <label>List of phone numbers</label>
+          <div>
+            {fields.map((field, idx) => {
+              return (
+                <div className="form-control" key={field.id}>
+                  <input
+                    type="text"
+                    {...register(`phnNumbers.${idx}.number` as const, {
+                      required: {
+                        value: idx === 0,
+                        message: "1 phone number is required",
+                      },
+                    })}
+                  />
+                  {idx === 0 && (
+                    <p className="error">
+                      {errors.phnNumbers?.[0]?.number &&
+                        errors.phnNumbers?.[0]?.number.message}
+                    </p>
+                  )}
+                  {idx > 0 && (
+                    <button type="button" onClick={() => remove(idx)}>
+                      Remove phone number
+                    </button>
+                  )}
+                </div>
+              );
+            })}
+            <button type="button" onClick={() => append({ number: "" })}>
+              Add phone number
+            </button>
+          </div>
         </div>
 
         <button>Submit</button>
